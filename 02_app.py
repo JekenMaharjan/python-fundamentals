256315246c717a1c35e0a33055604b7418eb9a75
JekenMaharjan <maharjanjeken@gmail.com>
2026-03-19 13:18:00 +0545

    02_app: find even numbers & count words in sentence

--- 02_app.py
+++ 02_app.py
@@ -1,11 +1,11 @@
-# =====================================================================
+# ==========================================================================================
 # Question 1: Sum of List
 # Problem: Write a function to find the sum of all numbers in a list.
 # Example:
     # Input: [1,2,3,4]
     # Output: 10
-# =====================================================================
-def sumOfList(arr):
+# ==========================================================================================
+def sum_list(arr):
     sum = 0
     
     for num in arr:
@@ -13,6 +13,37 @@ def sumOfList(arr):
         
     return sum
     
-print(f"Sum of all numbers in a list is {sumOfList([1,2,3,4])}")
+print(f"Sum of all numbers in a list is {sum_list([1,2,3,4])}")
 
-# =====================================================================
+# ==========================================================================================
+# Question 2: Find Even Numbers
+# Problem: Return only even numbers from a list.
+# Example:
+    # Input: [1,2,3,4,5,6]
+    # Output: [2,4,6]
+# ==========================================================================================
+def get_even(arr):
+    evens = []
+    
+    for num in arr:
+        if(num % 2 == 0):
+            evens.append(num)
+    
+    return evens
+
+print(f"Even numbers in given array : {get_even([1,2,3,4,5,6])}")
+
+# ==========================================================================================
+# Question 3: Count Words in Sentence
+# Problem: Count how many words are in a sentence.
+# Example:
+    # Input: "I love Python programming"
+    # Output: 4
+# ==========================================================================================
+def count_words(sentence):
+    words = sentence.split(" ")
+    return len(words)
+
+print(f"Words count : {count_words("I love Python programming")}")
+
+
